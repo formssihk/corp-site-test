5775b683b28875d7352d6bd7193f963f7474c5b2
Jacky Leung <jacky.leung@formssihk.com>
2024-08-15 17:49:28 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  distDir: "build",
   reactStrictMode: true,
   env: {
-    DIRECTUS_URL: "http://localhost:8080/api",
+    DIRECTUS_URL: "http://corporate-web/api",
     DIRECTUS_PUBLIC_ACCESS_TOKEN: "HuCcNoYJXYThNzY-opv_702Ax5hGGxim"
   }
 };
